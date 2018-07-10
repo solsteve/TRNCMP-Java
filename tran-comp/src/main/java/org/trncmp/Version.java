21b3efeb6c85865377e92e34b776fce9f49aca5d
Soliday <hpsp@soliday.com>
2018-07-10 16:57:35 -0400

    Review

--- tran-comp/src/main/java/org/trncmp/Version.java
+++
@@ -1,9 +0,0 @@
-package org.trncmp;
-public class Version {
-  public static final String number = "1.1.0";
-  public static final String date   = "2018-07-10T15:23:39Z";
-  public static final String name   = "com.l3.irad.thirdparty";
-  public static final String author = "Stephen W. Soliday";
-  public static final String email  = "mailto:stephen.soliday@trncmp.org";
-  public static final String www    = "http://research.trncmp.org";
-}
