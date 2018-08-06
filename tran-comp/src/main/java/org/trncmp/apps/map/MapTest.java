1404b4169ff368c03a2229e63181cd8cdbae2f07
Soliday <hpsp@soliday.com>
2018-08-06 10:50:04 -0400

    Adding prototype for map display

---
+++ tran-comp/src/main/java/org/trncmp/apps/map/MapTest.java
@@ -0,0 +1,29 @@
+package org.trncmp.apps.map;
+
+
+
+
+
+public class MapTest {
+
+
+ImageIcon image = new ImageIcon(this.getClass().getResource("INF1.png"));
+
+
+
+
+
+
+
+
+
+  public static void main( String[] args ) {
+
+
+
+
+
+    System.exit(0);
+  }
+
+} // end class MapTest
