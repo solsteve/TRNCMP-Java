2d9686c22a7fe10180248e302a8af9a7357b04c3
Brendan Waters <Brendan.Waters@L3T.com>
2019-03-08 13:15:29 -0500

    HDE-89: Large refactor - split trncmp-lib into three modules

--- tran-comp-lib/src/test/java/org/trncmp/lib/RunStatTest.java
+++ tran-comp-lib/src/test/java/org/trncmp/lib/RunStatTest.java
@@ -38,8 +38,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.trncmp.mllib.Entropy;
-
 // =======================================================================================
 public class RunStatTest {
   // -------------------------------------------------------------------------------------
